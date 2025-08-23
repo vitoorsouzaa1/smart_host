56bcda10a3b891e1345ce8f5ee0ac28dda3951e4
vituvitu <vitor.souza407@yahoo.com.br>
2025-08-23 01:43:06 -0300

    feat: add social button component with google and twitter support

---
+++ src/components/ui/SocialButton.tsx
@@ -0,0 +1,69 @@
+import { Button } from './button'
+
+interface SocialButtonProps {
+  provider: 'google' | 'twitter'
+  onClick?: () => void
+  disabled?: boolean
+}
+
+const socialIcons = {
+  google: (
+    <svg
+      className='w-5 h-5'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z'
+        fill='#4285F4'
+      />
+      <path
+        d='M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z'
+        fill='#34A853'
+      />
+      <path
+        d='M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z'
+        fill='#FBBC05'
+      />
+      <path
+        d='M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z'
+        fill='#EA4335'
+      />
+    </svg>
+  ),
+  twitter: (
+    <svg
+      className='w-5 h-5'
+      fill='currentColor'
+      viewBox='0 0 20 20'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path d='M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84' />
+    </svg>
+  ),
+}
+
+const socialLabels = {
+  google: 'Google',
+  twitter: 'Twitter',
+}
+
+export const SocialButton = ({
+  provider,
+  onClick,
+  disabled,
+}: SocialButtonProps) => {
+  return (
+    <Button
+      type='button'
+      variant='outline'
+      className='flex items-center justify-center w-full gap-2 hover:bg-gray-50 transition-colors'
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {socialIcons[provider]}
+      {socialLabels[provider]}
+    </Button>
+  )
+}
