a3c3ba437a2094cc1b15da5d98bb835fce37517e
vituvitu <vitor.souza407@yahoo.com.br>
2025-08-26 22:11:37 -0300

    build: add date picker and popover dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['example.com', 'images.unsplash.com', 'plus.unsplash.com']
-  }
-};
+    domains: ['example.com', 'images.unsplash.com', 'plus.unsplash.com'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
